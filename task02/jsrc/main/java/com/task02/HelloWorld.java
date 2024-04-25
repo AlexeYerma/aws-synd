1a29732ba3127e3f37e18b39cc71dbd829ef9292
Alexei Engineer <alexei.engineer@gmail.com>
2024-04-25 13:58:27 +0200

    task02

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import javax.ws.rs.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("hello")
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = true,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
+	}
+}
