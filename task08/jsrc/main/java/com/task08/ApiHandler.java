d9c70fc371422ea42dba17589b76b78b3bc74fbb
Alexei Engineer <alexei.engineer@gmail.com>
2024-05-11 12:45:51 +0200

    task08

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -11,19 +11,28 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "api_handler",
-	roleName = "api_handler-role",
+@LambdaHandler(
+		lambdaName = "api_handler",
+		roleName = "api_handler-role",
+		layers = {"sdk-layer"},
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 //	isPublishVersion = true,
 //	aliasName = "${lambdas_alias_name}",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
 )
 @LambdaLayer(
 		layerName = "sdk-layer",
