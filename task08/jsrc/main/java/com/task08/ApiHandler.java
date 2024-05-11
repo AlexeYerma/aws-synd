f0bdd45394d7d0fae797174c2391a11e67c7895b
Alexei Engineer <alexei.engineer@gmail.com>
2024-05-11 21:45:23 +0200

    task08

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -13,10 +13,12 @@ import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,11 +40,10 @@ import java.util.Map;
 //		libraries = {"target/original-task08-1.0.0.jar"},
 		libraries = {"lib/commons-lang3-3.14.0.jar"},
 		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64}
-//		artifactExtension = ArtifactExtension.ZIP
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
 )
 public class ApiHandler implements RequestHandler<Object, String> {
-
 
 	private static final HttpClient httpClient = HttpClient.newHttpClient();
 	private static final String OPEN_METEO_API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"; // Replace with actual Open-Meteo API URL
@@ -50,6 +51,8 @@ public class ApiHandler implements RequestHandler<Object, String> {
 	public String handleRequest(Object request, Context context) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		try {
+			String mock = StringUtils.EMPTY;
+			System.out.println("-F" + mock);
 			HttpRequest httpRequest = HttpRequest.newBuilder()
 					.GET()
 					.uri(URI.create(OPEN_METEO_API_URL))
