401c84fb4c700d4161f9214ed06e52a31be9b6ee
Alexei Engineer <alexei.engineer@gmail.com>
2024-05-20 19:48:05 +0200

    task10

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -6,8 +6,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -18,14 +16,14 @@ import java.util.Map;
 	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@DependsOn(
-		name = "Reservations",
-		resourceType = ResourceType.DYNAMODB_TABLE
-)
-@DependsOn(
-		name = "Tables",
-		resourceType = ResourceType.DYNAMODB_TABLE
-)
+//@DependsOn(
+//		name = "Reservations",
+//		resourceType = ResourceType.DYNAMODB_TABLE
+//)
+//@DependsOn(
+//		name = "Tables",
+//		resourceType = ResourceType.DYNAMODB_TABLE
+//)
 public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 	private AuthService authService = new AuthService();
 	private TableService tableService = new TableService();
